db925153bbe88aa3976b8f11c586bfb6f94efd73
Jeremiah Gowdy <jeremiah@gowdy.me>
2022-04-07 09:49:34 -0700

    Add echo statements

--- build-node-arm64-pac.sh
+++ build-node-arm64-pac.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+echo "Updating apt"
 apt-get update -y
+echo "Installing dependencies"
 apt-get install build-essential python3 python3-pip gcc-10-aarch64-linux-gnu g++-10-aarch64-linux-gnu binutils-aarch64-linux-gnu git -y
 
 VERSION=16.14.2
 TAG_VERSION=v${VERSION}
 
 rm -rf ./node
+echo "Cloning Node.js"
 git clone --depth 1 --branch ${TAG_VERSION} https://github.com/nodejs/node.git 
 cd node || exit 1
 
@@ -24,6 +27,8 @@ export CPP="aarch64-linux-gnu-cpp-10"
 export CFLAGS="${CFLAGS} -march=armv8.3-a -mbranch-protection=pac-ret "
 export CXXFLAGS="${CXXFLAGS} -march=armv8.3-a -mbranch-protection=pac-ret "
 
+echo "Building Node"
 ./configure && make -j8
+echo "Verifying output"
 stat ./node
 objdump --disassemble-all ./node | grep paciasap && echo PAC build successful && exit 0 || echo No PAC instructions found! && exit 1
