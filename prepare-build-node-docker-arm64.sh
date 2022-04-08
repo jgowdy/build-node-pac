6bdd06fa6c7d13aa18393f635d80ddec50a32336
Jeremiah Gowdy <jeremiah@gowdy.me>
2022-04-08 09:01:21 -0700

    Add clone to prepare

--- prepare-build-node-docker-arm64.sh
+++ prepare-build-node-docker-arm64.sh
@@ -14,5 +14,13 @@ docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 echo "Testing arm64v8 emulation"
 docker run --rm --platform linux/arm64/v8 -t arm64v8/ubuntu uname -m
 
+VERSION=16.14.2
+TAG_VERSION=v${VERSION}
+
+rm -rf ./node
+echo "Cloning Node.js"
+git clone --depth 1 --branch ${TAG_VERSION} https://github.com/nodejs/node.git 
+cd node || exit 1
+
 echo "Running configure in container"
 timeout 350m docker run --rm -v "$(pwd)":/build --platform linux/arm64/v8 -t arm64v8/ubuntu /build/in-container-configure-node.sh
