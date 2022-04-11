6a4fc232aad5d7df9faa0895d374478fd91d3b94
Jeremiah Gowdy <jeremiah@gowdy.me>
2022-04-11 15:04:01 -0700

    Not gcut

--- build-with-timeout-amazon.sh
+++ build-with-timeout-amazon.sh
@@ -2,10 +2,10 @@
 
 echo "Running build in container"
 
-docker ps | grep -v CONTAINER | tail -n 1 | gcut -f 1 -d ' ' | xargs docker unpause
+docker ps | grep -v CONTAINER | tail -n 1 | cut -f 1 -d ' ' | xargs docker unpause
 
 sleep 15
 
-docker ps | grep -v CONTAINER | tail -n 1 | gcut -f 1 -d ' ' | xargs docker pause
+docker ps | grep -v CONTAINER | tail -n 1 | cut -f 1 -d ' ' | xargs docker pause
 
 #timeout 350m docker run --rm -v "$(pwd)":/build --platform linux/arm64/v8 -t arm64v8/amazonlinux:2 /build/in-container-build-node-amazon.sh
