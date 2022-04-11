0b3d534fb469d3f5ec8a9428cc582d86c39126f7
Jeremiah Gowdy <jeremiah@gowdy.me>
2022-04-11 15:02:18 -0700

    Pause/unpause

--- build-with-timeout-amazon.sh
+++ build-with-timeout-amazon.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 echo "Running build in container"
-docker ps
+
+docker ps | grep -v CONTAINER | tail -n 1 | gcut -f 1 -d ' ' | xargs docker unpause
+
+sleep 15
+
+docker ps | grep -v CONTAINER | tail -n 1 | gcut -f 1 -d ' ' | xargs docker pause
 
 #timeout 350m docker run --rm -v "$(pwd)":/build --platform linux/arm64/v8 -t arm64v8/amazonlinux:2 /build/in-container-build-node-amazon.sh
